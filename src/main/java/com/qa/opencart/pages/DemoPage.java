b4732260173863e205358090ccd1ee024d40fb06
Vedaste Hasangwineza <vedastehasangwineza@Vedastes-MBP.attlocal.net>
2025-06-02 20:33:28 -0500

    Adding demo Page

---
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -0,0 +1,17 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class DemoPage {
+	
+	By demo = By.id("demo");
+	
+	
+
+public void getDemo() {
+	
+	System.out.println("demo details");
+	System.out.println(demo);
+}
+
+}
