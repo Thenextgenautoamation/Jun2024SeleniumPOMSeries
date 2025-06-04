a0744998f14165fbb75adba654e07a8eb6bfc5b5
Vedaste Hasangwineza <vedastehasangwineza@Vedastes-MBP.attlocal.net>
2025-06-03 22:05:17 -0500

    adding demo test page

---
+++ src/main/java/com/qa/opencart/pages/TestDemo.java
@@ -0,0 +1,21 @@
+package com.qa.opencart.pages;
+
+public class TestDemo {
+int b = 100;
+	
+	public void login() {
+		
+		System.out.println("this is login method");
+	}
+	
+	public void dologin() {
+		
+		System.out.println("dologin method");
+	}
+	
+	public void dosearch() {
+		
+		System.out.println("doSeach method");
+	
+	}
+}
